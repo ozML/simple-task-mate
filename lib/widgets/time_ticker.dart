e8ec0b70972f94948b17e928479b735bc9c36c34
Ruben Mikalay <ozml.pub@gmail.com>
2024-10-17 21:16:25 +0200

    Applied clock time format to time ticker

--- lib/widgets/time_ticker.dart
+++ lib/widgets/time_ticker.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_task_mate/models/config_model.dart';
 import 'package:simple_task_mate/utils/theme_utils.dart';
 
 class TimeTicker extends StatelessWidget {
@@ -8,8 +10,28 @@ class TimeTicker extends StatelessWidget {
   final DateTime time;
 
   @override
-  Widget build(BuildContext context) => Text(
-        DateFormat('HH:mm:ss').format(time),
-        style: bigPrimaryTextStyleFrom(context),
-      );
+  Widget build(BuildContext context) {
+    final config = context.watch<ConfigModel>();
+    final clockTimeFormatConfigValue = config.getValue(settingClockTimeFormat);
+    final clockTimeFormat = switch (clockTimeFormatConfigValue) {
+      ClockTimeFormat.twelveHours => 'hh:mm:ss',
+      _ => 'HH:mm:ss',
+    };
+    final languageCode = config.getValue<Locale>(settingLanguage).languageCode;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          DateFormat(clockTimeFormat, languageCode).format(time),
+          style: bigPrimaryTextStyleFrom(context),
+        ),
+        if (clockTimeFormatConfigValue == ClockTimeFormat.twelveHours)
+          Text(
+            DateFormat('a', languageCode).format(time),
+            style: secondaryTextStyleFrom(context),
+          ),
+      ],
+    );
+  }
 }
