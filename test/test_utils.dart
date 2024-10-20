2c903e69a4536ec248884c74445ea3c2487aa3c9
Ruben Mikalay <ozml.pub@gmail.com>
2024-10-20 07:14:38 +0200

    Added tests for title bands

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:simple_task_mate/services/api.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class TestApp extends StatelessWidget {
   const TestApp({required this.child, super.key});
@@ -7,6 +10,9 @@ class TestApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: const Locale('en'),
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
@@ -24,4 +30,37 @@ final testWeek = [
   DateTime(2024, 10, 18),
   DateTime(2024, 10, 19),
   DateTime(2024, 10, 20),
+];
+
+final testStamps = [
+  Stamp(type: StampType.arrival, time: testWeek.first.copyWith(hour: 8)),
+  Stamp(
+    type: StampType.departure,
+    time: testWeek.first.copyWith(hour: 12, minute: 30),
+  ),
+  Stamp(type: StampType.arrival, time: testWeek.first.copyWith(hour: 13)),
+  Stamp(
+    type: StampType.departure,
+    time: testWeek.first.copyWith(hour: 16, minute: 30),
+  ),
+];
+
+final testFullTasks = [
+  Task(
+    id: 0,
+    name: 'task',
+    entries: [
+      TaskEntry(
+        taskId: 0,
+        date: testWeek.first,
+        duration: const Duration(hours: 4),
+        info: 'First entry',
+      ),
+      TaskEntry(
+        taskId: 0,
+        date: testWeek.first,
+        duration: const Duration(hours: 2, minutes: 30),
+      ),
+    ],
+  ),
 ];
