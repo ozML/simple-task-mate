a885d98444c43cdb07d20919882cee130af177b0
Ruben Mikalay <ozml.pub@gmail.com>
2025-03-02 20:34:12 +0100

    Keep db open during active access

--- lib/database/data_base_helper.dart
+++ lib/database/data_base_helper.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 abstract class DataBaseHelper {
   DataBaseHelper({required this.dataBasePath});
+
+  static int accessCounter = 0;
 
   final String dataBasePath;
 
@@ -11,11 +14,21 @@ abstract class DataBaseHelper {
   Future<T?> dbAction<T>(Future<T?> Function(Database db) action) async {
     Database? db;
     try {
+      accessCounter++;
+
       db = await openConnection();
       return action(db);
     } on Exception catch (_) {
     } finally {
-      db?.close();
+      accessCounter--;
+
+      if (accessCounter == 0) {
+        db?.close();
+      } else if (accessCounter < 0) {
+        if (kDebugMode) {
+          print('access counter became negative');
+        }
+      }
     }
 
     return null;
